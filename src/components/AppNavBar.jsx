700a525ad935aa7fc6aa1c00d2ff26227c5ba7eb
D4NTE-DB <dantebabilonia98@gmail.com>
2023-03-09 15:54:08 -0500

    update

--- src/components/AppNavBar.jsx
+++ src/components/AppNavBar.jsx
@@ -6,7 +6,7 @@ const AppNavBar = () => {
         <div>
             <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Guia-Sincelejo</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">

85487c8580a8f225c71004fa3aa0c908f90f014b
D4NTE-DB <dantebabilonia98@gmail.com>
2023-03-10 01:41:03 -0500

    update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -13,7 +13,7 @@ const Cards = () => {
                 <Card  style={{ width: '10rem', height: '25rem' }}>
                     <Card.Img style={{width: '160px', height: '160px'}}  variant="top" src={data.images} />
                     <Card.Body>
-                        <Card.Title>{data.name}</Card.Title>
+                        <Card.Title style={{width: '128px', height: '50px'}}>{data.name}</Card.Title>
                         {/* <Card.Text>
                             Some quick example text to build on the card title and make up the
                             bulk of the card's content.

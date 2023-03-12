ce3d0164f6fd2f2a2525fcecb2506cfd14d6b7ef
D4NTE-DB <dantebabilonia98@gmail.com>
2023-03-12 02:15:06 -0500

    update

--- src/images.jsx
+++ src/images.jsx
@@ -8,7 +8,7 @@ const DATA = [
         contact: {
             instgram: "https://www.instagram.com/fonda_mrlimon/",
             phoneNum: "https://wa.link/qpxehq"
-        }   
+        }
     },
 
     {
@@ -41,21 +41,113 @@ const DATA = [
             phoneNum: "https://wa.link/4xaebs"
         }
     },
-   
-   
 
+    {
+        id: '4',
+        name: 'La BBH',
+        category: "Comida Rápida",
+        images: 'images/6.jpg',
+        contact: {
+            instgram: "instagram.com/_labbh_/",
+            phoneNum: "wa.me/message/KHTKAOVZVZV3M1"
+        }
+    },
+    {
+        id: '5',
+        name: 'Taco y Chela',
+        category: " Comida Mexicana",
+        images: 'images/5.jpeg',
+        contact: {
+            instgram: "https://www.instagram.com/tacoychela.co/",
+            phoneNum: "linkbe.me/tacoychela.co"
+        }
+    },
+    {
+        id: '6',
+        name: 'KFV',
+        category: "Comidas Rápidas",
+        images: 'images/8.jfif',
+        contact: {
+            instgram: "https://www.instagram.com/kfvrestaurante/",
+            phoneNum: "https://wa.link/47em9zhttps://wa.link/47em9z"
+        }
+    },
+    {
+        id: '7',
+        name: 'Place Arepas',
+        category: "Arepas",
+        images: 'images/9.jpg',
+        contact: {
+            instgram: "https://www.instagram.com/placearepas/",
+            phoneNum: "https://wa.link/voi51s"
+        }
+    },
+    {
+        id: '8',
+        name: 'La Mondiu Burger',
+        category: "Comida Rápida",
+        images: 'images/10.jpg',
+        contact: {
+            instgram: "instagram.com/lamondiu.burger/",
+            phoneNum: "bio.site/lamondiu"
+        }
+    },
+
+    {
+        id: '9',
+        name: 'Mr.Burger',
+        category: "Comida Rápida",
+        images: 'images/11.enc',
+        contact: {
+            instgram: "https://www.instagram.com/mrburgersjo/",
+            phoneNum: "https://bio.site/lamondiu"
+        }
+    },
+
+    {
+        id: '10',
+        name: 'Pepitos',
+        category: "Comida Rápida",
+        images: 'images/12.png',
+        contact: {
+            instgram: "https://www.instagram.com/pepitos_xpress/",
+            phoneNum: "https://pepitos-xpress.cluvi.co/branch-without-service"
+        }
+    },
+    {
+        id: '11',
+        name: 'Hunter & Butcher',
+        category: "Hamburguesas",
+        images: 'images/13.png',
+        contact: {
+            instgram:  "https://www.instagram.com/hunterbutcher.co/",
+            phoneNum: "https://linktr.ee/hunterbutcher"
+        }
+    }, 
+    
+    {
+        id: '12',
+        name: 'Pizza House Col',
+        category: "Pizzas",
+        images: 'images/14.png',
+        contact: {
+            instgram:  "https://www.instagram.com/pizzahousecol/",
+            phoneNum: "https://beacons.ai/pizzahousecol/"
+        }
+    }, 
+    {
+        id: '13',
+        name: 'Gastro Lab',
+        category: "Comidas Rápidas",
+        images: 'images/15.jfif',
+        contact: {
+            instgram:  "https://www.instagram.com/gastroolab/",
+            phoneNum: "https://linktr.ee/gastroolab"
+        }
+    }, 
 ]
 
-        // {
-        //     id: '',
-        //     name: '',
-        //     category: "",
-        //     images: '',
-        //     contact: {
-        //         instgram: "",
-        //         phoneNum: ""
-        //     }   
-        // }
+
 
 
 export default DATA;
